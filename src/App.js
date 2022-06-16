5ebd1a80eb67ad4c562c071cbed99d94fc0df9e4
Soumali <gorai.soumali@gmail.com>
2022-06-16 19:50:02 +0530

    Navbar banachi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+     <Routes>
+      <Route path="/" element={<Home />} />
+     </Routes>
+     </>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavbarCompo from "../components/Navbar";
+function Home() {
+  return (
+    <div> 
+    <NavbarCompo />
+    </div>
+  )
+}
+
+export default Home
