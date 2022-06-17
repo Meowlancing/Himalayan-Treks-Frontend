187392735c50769776c8d9c699609ebbb4ef3b62
Soumali <gorai.soumali@gmail.com>
2022-06-17 20:01:41 +0530

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles/header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div>
+        <h1 className="text">India's</h1> <br />
+        <h1 className="text1">Trekking Expert</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../components/Header';
 import NavbarCompo from "../components/Navbar";
 function Home() {
   return (
     <div> 
     <NavbarCompo />
+    <Header />
     </div>
   )
 }
