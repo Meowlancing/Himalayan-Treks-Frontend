77f7bbbe06d2ae8d4e5929dafed47eca57d0c176
Soumali <gorai.soumali@gmail.com>
2022-06-18 17:33:47 +0530

    ..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import { Button } from "react-bootstrap";
 import React from "react";
 import Header from "../components/Header";
 import NavbarCompo from "../components/Navbar";
+import "./styles/home.css"
+
 function Home() {
   return (
     <div>
       <NavbarCompo />
       <Header />
+      <Button className="treksBtn">View All</Button>
     </div>
   );
 }
