25fe52ccc289b98182a457563d13cd080e6bde14
Soumali Gorai <gorai.soumali@gmail.com>
2022-06-22 19:00:40 +0530

    ..

--- src/components/ClientTestimonials.jsx
+++ src/components/ClientTestimonials.jsx
@@ -3,51 +3,74 @@ import { Card, Carousel } from "react-bootstrap";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./styles/test.css";
 
-
 function ClientTestimonials() {
   return (
-      <div className="TestimonialSection">
-        <Card className="TestCardComponent">
-          <Card.Img className="CardImages" variant="top" src="https://preview.redd.it/rnb3syrx44c81.png?auto=webp&s=60f049e33b26f62ef27ef0b72f40d56bb49e3e2c" />
-          <Card.Body>
-            <Card.Title>Genshin Impact</Card.Title>
-            <Card.Title>🌟🌟🌟🌟🌟</Card.Title>
-            <Card.Text style={{margin:"0"}}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="TestCardComponent">
-          <Card.Img className="CardImages" variant="top" src="https://preview.redd.it/rnb3syrx44c81.png?auto=webp&s=60f049e33b26f62ef27ef0b72f40d56bb49e3e2c" />
-          <Card.Body>
-            <Card.Title>Genshin Impact</Card.Title>
-            <Card.Title>🌟🌟🌟🌟🌟</Card.Title>
-            <Card.Text style={{margin:"0"}}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="TestCardComponent">
-          <Card.Img className="CardImages" variant="top" src="https://preview.redd.it/rnb3syrx44c81.png?auto=webp&s=60f049e33b26f62ef27ef0b72f40d56bb49e3e2c" />
-          <Card.Body>
-            <Card.Title>Genshin Impact</Card.Title>
-            <Card.Title>🌟🌟🌟🌟🌟</Card.Title>
-            <Card.Text style={{margin:"0"}}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="TestCardComponent">
-          <Card.Img className="CardImages" variant="top" src="https://preview.redd.it/rnb3syrx44c81.png?auto=webp&s=60f049e33b26f62ef27ef0b72f40d56bb49e3e2c" />
-          <Card.Body>
-            <Card.Title>Genshin Impact</Card.Title>
-            <Card.Title>🌟🌟🌟🌟🌟</Card.Title>
-            <Card.Text style={{margin:"0"}}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
+    <div className="TestimonialSection">
+      <Card className="TestCardComponent">
+        <Card.Img
+          className="CardImages"
+          variant="top"
+          src="https://preview.redd.it/rnb3syrx44c81.png?auto=webp&s=60f049e33b26f62ef27ef0b72f40d56bb49e3e2c"
+        />
+        <Card.Body>
+          <Card.Title className="title">Genshin Impact</Card.Title>
+          <Card.Title className="stars">⭐⭐⭐⭐⭐</Card.Title>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam
+          </span>
+        </Card.Body>
+      </Card>
+      <Card className="TestCardComponent">
+        <Card.Img
+          className="CardImages"
+          variant="top"
+          src="https://preview.redd.it/rnb3syrx44c81.png?auto=webp&s=60f049e33b26f62ef27ef0b72f40d56bb49e3e2c"
+        />
+        <Card.Body>
+          <Card.Title className="title">Genshin Impact</Card.Title>
+          <Card.Title className="stars">⭐⭐⭐⭐⭐</Card.Title>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam
+          </span>
+        </Card.Body>
+      </Card>
+      <Card className="TestCardComponent">
+        <Card.Img
+          className="CardImages"
+          variant="top"
+          src="https://preview.redd.it/rnb3syrx44c81.png?auto=webp&s=60f049e33b26f62ef27ef0b72f40d56bb49e3e2c"
+        />
+        <Card.Body>
+          <Card.Title className="title">Genshin Impact</Card.Title>
+          <Card.Title className="stars">⭐⭐⭐⭐⭐</Card.Title>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam
+          </span>
+        </Card.Body>
+      </Card>
+      <Card className="TestCardComponent">
+        <Card.Img
+          className="CardImages"
+          variant="top"
+          src="https://preview.redd.it/rnb3syrx44c81.png?auto=webp&s=60f049e33b26f62ef27ef0b72f40d56bb49e3e2c"
+        />
+        <Card.Body>
+          <Card.Title className="title">Genshin Impact</Card.Title>
+          <Card.Title className="stars">⭐⭐⭐⭐⭐</Card.Title>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam,
+          </span>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
