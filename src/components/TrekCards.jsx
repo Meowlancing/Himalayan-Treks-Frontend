d4b4a0dfa3a07d1def1262c852dc7225d5166068
Debdeep1 <deb.baner.1@gmail.com>
2022-06-27 09:50:59 +0530

    treks page

--- src/components/TrekCards.jsx
+++ src/components/TrekCards.jsx
@@ -1,28 +1,25 @@
 import React from "react";
-import "./styles/trekcards.css";
-
+import "./styles/treks.css";
+import TREKCardCompo from "./TREKCardCompo"
 function TrekCards() {
   return (
-    <div className="treks">
-      <div className="trekCard">
-        <a href="/treks-id">
-          <img src="https://himalayandreamtreks.in/wp-content/uploads/2021/03/Kedarkantha-min-1.jpg" />
-        </a>
-        <div className="topLeft">
-          <span>05 </span>
-          <span>Days</span>
-        </div>
-        <div className="topRight">
-          <span>Rs. </span>
-          <span>7999</span>
-        </div>
-        <div className="bottom">
-          <a href="/treks-id">
-            <button className="trekName">Kedarkantha Trek </button>
-          </a>
-        </div>
-      </div>
-    </div>
+    <section className="Treks">
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/Kedarkantha-min-1.jpg" days="05" rate="8,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/Bali-Pass-min-1.jpg" days="08" rate="18,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/Dayara-Bugyal-min-1.jpg" days="09" rate="18,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/Black-Peak-min-1.jpg" days="15" rate="80,000"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/VALLEY-OF-FLOWERS-2.jpg" days="06" rate="9,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/FB_IMG_16155292870173129-min.jpg" days="08" rate="13,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/20190906161545_IMG_0214-min.jpg" days="04" rate="11,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/GAUMUKH-TAPOVAN-7.jpg" days="08" rate="19,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/LAMKHAGA-7.jpg" days="09" rate="39,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/Kedarkantha-6.jpg" days="07" rate="19,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/HAR-KI-dOON-11.jpg" days="08" rate="10,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/KUARI-PASS-13.jpg" days="06" rate="11,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2022/05/20210930_110337-01.jpeg" days="08" rate="18,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2022/05/IMG_4482.jpg" days="06" rate="14,999"/>
+      <TREKCardCompo img="https://himalayandreamtreks.in/wp-content/uploads/2021/03/HAR-KI-dOON-11.jpg" days="06" rate="9,999"/>
+    </section>
   );
 }
 
