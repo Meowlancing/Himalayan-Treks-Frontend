493ce2342cab90ef8b4793bfc13730b0cf4b8c8e
Soumali Gorai <gorai.soumali@gmail.com>
2022-06-30 15:48:12 +0530

    data

--- src/components/TrekCards.jsx
+++ src/components/TrekCards.jsx
@@ -8,40 +8,44 @@ function TrekCards() {
     try {
       const resp = await axios({
         method: "GET",
-        url: "http://localhost8000:/api/treksList",
+        url: "http://localhost:8000/api/treksList",
       });
-      console.log(resp.data);
       setData(resp.data);
     } catch (err) {
       alert("Something went wrong");
       return [];
     }
   }
+  console.log(data);
   useEffect(() => {
     getData();
   }, []);
-  return (
-    <div className="treks">
-      <div className="trekCard">
-        <a href="/treks-id">
-          <img src="https://himalayandreamtreks.in/wp-content/uploads/2021/03/Kedarkantha-min-1.jpg" />
-        </a>
-        <div className="topLeft">
-          <span>05 </span>
-          <span>Days</span>
-        </div>
-        <div className="topRight">
-          <span>Rs. </span>
-          <span>8,999</span>
-        </div>
-        <div className="bottom">
+
+
+  return data.map((item) => {
+    return (
+      <div className="treks">
+        <div className="trekCard">
           <a href="/treks-id">
-            <button className="trekName">{data.title}</button>
+            <img src="https://himalayandreamtreks.in/wp-content/uploads/2021/03/Kedarkantha-min-1.jpg" />
           </a>
+          <div className="topLeft">
+            <span>05 </span>
+            <span>Days</span>
+          </div>
+          <div className="topRight">
+            <span>Rs. </span>
+            <span>8,999</span>
+          </div>
+          <div className="bottom">
+            <a href="/treks-id">
+              <button className="trekName">{item.title}</button>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  });
 }
 
 export default TrekCards;
